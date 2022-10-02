f94c277c4ec7f72048ff5b149a05cdc47bd0c1c7
vicky109717 <vignesh109717@gmail.com>
2022-10-02 16:12:10 +0530

    code added

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,12 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	private void deposit() {
+		// TODO Auto-generated method stub
+System.out.println("5");
+	}
+	public static void main(String[] args) {
+		AxisBank a = new AxisBank();
+		a.deposit();a.saving();a.fixed();
+	}
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,12 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		// TODO Auto-generated method stub
+System.out.println("2");
+	}
+	public void fixed() {
+		// TODO Auto-generated method stub
+System.out.println("4");
+	}
+}
